e4ec8437b3c90ad20fb09fddd5b4af8bc22e186b
Sodam Park <parksodam0228@e-mirim.hs.kr>
2017-09-21 16:48:48 +0900

    메인 화면 디자인

--- 2509_박소담_C++_Project/main.cpp
+++ 2509_박소담_C++_Project/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include<conio.h>
 using namespace std;
 
 void gotoxy(int x, int y) {
@@ -8,31 +9,54 @@ void gotoxy(int x, int y) {
 	pos.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+void startMain(){
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "                               ■      ■   ■      ■      ■      ■         ■          ■■■■      ■    ■■■■" << endl;
+	cout << "                               ■■  ■■    ■    ■       ■■  ■■        ■■        ■             ■    ■    ■" << endl;
+	cout << "                               ■ ■■ ■     ■  ■        ■ ■■ ■       ■  ■      ■              ■    ■" << endl;
+	cout << "                               ■  ■  ■       ■          ■  ■  ■      ■■■■     ■    ■■■    ■    ■" << endl;
+	cout << "                               ■      ■       ■          ■      ■     ■      ■    ■      ■      ■    ■" << endl;
+	cout << "                               ■      ■       ■          ■      ■    ■        ■    ■     ■      ■    ■    ■ " << endl;
+	cout << "                               ■      ■       ■          ■      ■   ■          ■    ■■■■      ■    ■■■■" << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "                                                          ■■■■           ■       ■      ■" << endl;
+	cout << "                                                          ■     ■         ■■       ■    ■" << endl;
+	cout << "                                                          ■     ■        ■  ■       ■  ■" << endl;
+	cout << "                                                          ■     ■       ■■■■        ■" << endl;
+	cout << "                                                          ■     ■      ■      ■       ■" << endl;
+	cout << "                                                          ■     ■     ■        ■      ■" << endl;
+	cout << "                                                          ■■■■     ■          ■     ■" << endl;
+}
+
+void selectMenu(){
+	
+	startMain();
+	gotoxy(70, 33);
+	cout << "1. 생리일 확인하기" << endl;
+	gotoxy(70, 36);
+	cout << "2. 가임기 확인하기" << endl;
+	gotoxy(70, 39);
+	cout << "3. 사용방법" << endl;
+	gotoxy(70, 42);
+	cout << "4. 종료하기" << endl;
+}
 
 int main(void){
-	system("mode con: cols=100 lines=40");
+	system("mode con: cols=150 lines=50");
 	
-	for (int i = 3; i < 9; i++){
-		gotoxy(30, i);
-		cout << "■" << endl;
-	}
-
-	for (int i = 3; i < 9; i++){
-		gotoxy(42, i);
-		cout << "■" << endl;
-	}
-
-	for (int i = 3; i < 9; i++){
-		gotoxy(47, i);
-		cout << "■" << endl;
-	}
-
-	for (int i = 3; i < 9; i++){
-		gotoxy(47, i);
-		cout << "■" << endl;
-	}
- 
-
+	selectMenu();
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
 	return 0;
 }
 
